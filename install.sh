b1881d077c82b67a1c100290ee688712b5059b76
Yotam Harchol <yotamhc@gmail.com>
2016-01-03 22:18:42 -0800

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo '[+] Installing NodeJS'
+sudo apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 echo '[+] Initializing Node Application'
 npm init
 
